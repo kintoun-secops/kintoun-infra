b788b0475dd5c8ef6d8e76829ad54a9bce188c3f
Park123r <jerom29@naver.com>
2026-09-09 00:39:13 +0900

    feat:Wazuh-logging 루트모듈 추가 및 GuardDuty 및 Waf-log 연동

--- wazuh-logging/guardduty/remote_state.tf
+++ wazuh-logging/guardduty/remote_state.tf
@@ -1,12 +1,12 @@
 # =======================================================
 # platform/ 모듈의 tfstate에서 가져오기
 # =======================================================
-data "terraform_remote_state" "platform" { # 이건 남의 state를 읽어오는 것이다.
+data "terraform_remote_state" "wazuh" { # 이건 남의 state를 읽어오는 것이다.
   backend = "s3"
 
   config = {
     bucket = "kintoun-tfstate"
-    key    = "platform/terraform.tfstate"
+    key    = "platform/wazuh/terraform.tfstate"
     region = "ap-northeast-2"
   }
 }
@@ -15,8 +15,8 @@ data "terraform_remote_state" "platform" { # 이건 남의 state를 읽어오는
 # platform 모듈의 Wazuh Role Name, Wazuh EC2 ARN
 # =======================================================
 locals {
-  wazuh_role    = data.terraform_remote_state.platform.outputs.wazuh_role_name
-  wazuh_ec2_arn = data.terraform_remote_state.platform.outputs.wazuh_ec2_arn
+  wazuh_role    = data.terraform_remote_state.wazuh.outputs.wazuh_role_name
+  wazuh_ec2_arn = data.terraform_remote_state.wazuh.outputs.wazuh_ec2_arn
 }
 # variable로는 remote_state의 outputs를 data로 불러오는게 불가능
 # =======================================================
