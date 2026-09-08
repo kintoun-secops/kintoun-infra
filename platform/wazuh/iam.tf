77f8d50bb02249548722531f57fb3ec57d0f2e1b
taehyung-99 <kth72005@gmail.com>
2026-09-08 22:42:44 +0900

    fix: import 선언문제로 policy name 변경이 불가능해서 정책 신규 생성후 연결

--- platform/wazuh/iam.tf
+++ platform/wazuh/iam.tf
@@ -55,12 +55,26 @@ resource "aws_iam_policy" "ssm_role" {
   }
 }
 
+resource "aws_iam_policy" "wazuh_ssm_role" {
+  name        = "${var.project_name}-wazuh-ec2-ssm-role"
+  description = "Enable SSM management for Wazuh EC2"
+  policy      = data.aws_iam_policy_document.ssm_role.json
+
+  tags = {
+    Name = "${var.project_name}-wazuh-ec2-ssm-role"
+  }
+}
+
 # =======================================================
 # IAM Role에 권한정책 연결
 # =======================================================
 resource "aws_iam_role_policy_attachment" "wazuh_ssm" {
   role       = aws_iam_role.wazuh_role.name
   policy_arn = aws_iam_policy.ssm_role.arn
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # =======================================================
